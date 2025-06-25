19a53de88f64cce9112c3845dd993a9f9fa49dee
Arpit06k <158275445+Arpit06k@users.noreply.github.com>
2025-06-25 15:25:47 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (43.52%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,28 +11,52 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
-            vector<vector<int>>ans;
-        if(root==NULL)return 0;
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<int>level;
-        while(!q.empty()){
-            int size1=q.size();
-            
-            for(int i=0;i<size1;i++){
-                 TreeNode* node=q.front();
-                q.pop();
-                   level.push_back(node->val); 
-                if(node->left!=NULL){
-                    q.push(node->left);
-                }
-                  if(node->right!=NULL){
-                    q.push(node->right);
-                }
-                              }
-                            ans.push_back(level);
+    int leftheight(TreeNode* node){
+        int h=0;
+        while(node){
+            h++;
+            node=node->left;
         }
-       return level.size();
+        return h;
+    }
+    int rightheight(TreeNode* node){
+        int h=0;
+        while(node){
+            h++;
+            node=node->right;
+        }
+        return h;
+    }
+    int countNodes(TreeNode* root) {
+        if(root ==NULL)return 0;
+        int lh=leftheight(root);
+        int rh=rightheight(root);
+
+        if(lh==rh)return (1<<lh)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
+
+    //     vector<vector<int>>ans;
+    //     if(root==NULL)return 0;
+    //     queue<TreeNode*>q;
+    //     q.push(root);
+    //     vector<int>level;
+    //     while(!q.empty()){
+    //         int size1=q.size();
+            
+    //         for(int i=0;i<size1;i++){
+    //              TreeNode* node=q.front();
+    //             q.pop();
+    //                level.push_back(node->val); 
+    //             if(node->left!=NULL){
+    //                 q.push(node->left);
+    //             }
+    //               if(node->right!=NULL){
+    //                 q.push(node->right);
+    //             }
+    //                           }
+    //                         ans.push_back(level);
+    //     }
+    //    return level.size();
+    // }
