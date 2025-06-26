8f8fe140ef59fb2d8c5f454000aeedf47dbab013
Arpit06k <158275445+Arpit06k@users.noreply.github.com>
2025-06-26 22:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (64.50%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -12,25 +12,53 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-         vector<int>inorder;
-        stack<TreeNode*>st;
-        TreeNode* node=root;
-        while(true){
-            if(node!=NULL){
-                st.push(node);
-                node=node->left;
+       vector<int>inorder;
+       TreeNode* curr=root;
+       while(curr!=NULL){
+        if(curr->left==NULL){
+            inorder.push_back(curr->val);
+            curr=curr->right;
+        }
+        else{
+            TreeNode* prev=curr->left;
+            while(prev->right&&prev->right!=curr){
+                prev=prev->right;
+
+            }
+            if(prev->right==NULL){
+                prev->right=curr;
+                curr=curr->left;
             }
             else{
-                if(st.empty())break;
-                node=st.top();
-                st.pop();
-                inorder.push_back(node->val);
-                node=node->right;
+                prev->right=NULL;
+                inorder.push_back(curr->val);
+                curr=curr->right;
             }
+        }
+       }
+        return inorder;
+       }
+    
+    
+};
+
+
+//   vector<int>inorder;
+//         stack<TreeNode*>st;
+//         TreeNode* node=root;
+//         while(true){
+//             if(node!=NULL){
+//                 st.push(node);
+//                 node=node->left;
+//             }
+//             else{
+//                 if(st.empty())break;
+//                 node=st.top();
+//                 st.pop();
+//                 inorder.push_back(node->val);
+//                 node=node->right;
+//             }
         
        
-        }
-        return inorder;
-    
-    }
-};
+//         }
+//         return inorder;
