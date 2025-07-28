8b4bc6e17e2416d03a16a9ebe324dbd07bcb051a
Arpit06k <158275445+Arpit06k@users.noreply.github.com>
2025-07-28 16:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (84.57%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -15,24 +15,46 @@ public:
     // }
             //    return paths(m-1,n-1,dp);
 
+//     int uniquePaths(int m, int n) {
+//             vector<vector<int>>dp(m,vector<int>(n));
+//         //tabulation
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 && j==0){
+//                     dp[0][0]=1;
+//                 }
+//                 else{
+//                     int up=0;
+//                     int left=0;
+//                     if(i>0)up=dp[i-1][j];
+//                     if(j>0) left=dp[i][j-1];
+//                     dp[i][j]=up+left;
+//                 }
+//             }
+//         }
+// return dp[m-1][n-1];
+//     }
+// };
+
     int uniquePaths(int m, int n) {
-            vector<vector<int>>dp(m,vector<int>(n));
-        //tabulation
+        vector<int>prev(n,0);
         for(int i=0;i<m;i++){
+            vector<int>temp(n,0);
             for(int j=0;j<n;j++){
                 if(i==0 && j==0){
-                    dp[0][0]=1;
+                    temp[j]=1;
                 }
                 else{
-                    int up=0;
+                     int up=0;
                     int left=0;
-                    if(i>0)up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-                    dp[i][j]=up+left;
+                    if(i>0)up=prev[j];
+                    if(j>0) left=temp[j-1];
+                    temp[j]=up+left;
                 }
+                
             }
-        }
-return dp[m-1][n-1];
+prev=temp;
+  }
+  return prev[n-1];
     }
 };
-
