9a25a646283581ae306e27aff50cf9bdeb2a20fc
Arpit06k <158275445+Arpit06k@users.noreply.github.com>
2025-10-23 21:07:37 +0530

    Time: 66 ms (10.22%) | Memory: 66.3 MB (37.22%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    bool dfs(int node,int n,vector<vector<int>>& graph,vector<int>&vis,vector<int>&pathvis,vector<int>&safe){
-        vis[node]=1;
-        pathvis[node]=1;
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+        for (int i = 0; i < n; i++) {
+            for (auto it : graph[i]) {
+                adj[it].push_back(i);
+                indegree[i]++;
+            }
+        }
 
-        for(auto it: graph[node]){
-            if(!vis[it]){
-                if(dfs(it,n,graph,vis,pathvis,safe)==true){
-                    return true;
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
                 }
             }
-            else if(pathvis[it]){
-                return true;
-            }
         }
-        safe[node]=1;
-        pathvis[node]=0;
-        return false;
-    }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>vis(n,0);
-        vector<int>pathvis(n,0);
+        sort(topo.begin(),topo.end());
 
-        vector<int>ans;
-        vector<int>safe(n,0);
-
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,n,graph,vis,pathvis,safe);
-            }
-        }
-
-        for(int i=0;i<n;i++){
-            if(safe[i]==1){
-                ans.push_back(i);
-            }
-        }
-        return ans;
+        return topo;
     }
 };
